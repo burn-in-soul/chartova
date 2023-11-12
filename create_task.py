c89b597ebee8625e7731db429897dd598f47ab4d
Alexander <csmcgl@mail.ru>
2023-11-12 23:36:30 +0300

    rm test task

--- create_task.py
+++ create_task.py
@@ -5,8 +5,6 @@ from celery_app import vote_pack, test_task
 def create_tasks(track_id: int, intervals: List[float]) -> None:
     countdown = 0
     for interval in intervals:
-        # vote_pack.apply_async(countdown=countdown,
-        #                       kwargs={'track_id': track_id})
-        test_task.apply_async(countdown=countdown,
+        vote_pack.apply_async(countdown=countdown,
                               kwargs={'track_id': track_id})
         countdown += interval
