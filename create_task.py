a13a79aafcd8d38a4a8a61a30aa8ced78d37d167
Alexander <csmcgl@mail.ru>
2023-11-12 23:47:35 +0300

    use tz

--- create_task.py
+++ create_task.py
@@ -1,10 +1,17 @@
+from datetime import datetime, timedelta
 from typing import List
+
+from pytz import timezone
+
 from celery_app import vote_pack, test_task
+
+msc_tz = timezone('Europe/Moscow')
 
 
 def create_tasks(track_id: int, intervals: List[float]) -> None:
     countdown = 0
     for interval in intervals:
-        vote_pack.apply_async(countdown=countdown,
-                              kwargs={'track_id': track_id})
+        vote_pack.apply_async(
+            eta=msc_tz.localize(datetime.now()) + timedelta(countdown),
+            kwargs={'track_id': track_id})
         countdown += interval
