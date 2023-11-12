4a6e59a089827a0c2f936541a60d2df1b64cee24
Alexander <csmcgl@mail.ru>
2023-11-13 00:01:13 +0300

    prepare with proxy

--- vote/voter.py
+++ vote/voter.py
@@ -19,7 +19,6 @@ class Voter:
         self._session = requests.Session()
         self._session.proxies = {'http': config.TOR_PROXY,
                                  'https': config.TOR_PROXY}
-        self._prepare_data()
 
     def vote_pack(self, track_id: int) -> None:
         print('run task')
@@ -28,6 +27,8 @@ class Voter:
                 port=config.TOR_PORT) as controller:
             controller.authenticate(password=config.TOR_PASSWORD)
             print('get controller')
+            self._prepare_data()
+            print('prepare data')
             one_vote = Vote(session=self._session, headers=self._headers)
             ip = IpService(session=self._session).check()
             print(f'Start with ip: {ip}')
@@ -43,7 +44,7 @@ class Voter:
             'User-Agent': UserAgent().random
         }
         response = self._session.get('https://www.nashe.ru/chartova/',
-                                     proxies={}, timeout=10)
+                                     timeout=10)
         parser = Parser(response.content)
         self._headers['X-CSRF-TOKEN'] = parser.get_csrf()
         self.iteration_id = parser.get_iteration_id()
