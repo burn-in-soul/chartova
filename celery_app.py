edbd4e2fc5e3c417a41b9ab942d2464746093ca0
Alexander <csmcgl@mail.ru>
2023-11-12 14:02:31 +0300

    rm python-dotenv

--- celery_app.py
+++ celery_app.py
@@ -1,10 +1,7 @@
 from celery import Celery
-from dotenv import load_dotenv
 
 import config
 from vote.voter import Voter
-
-load_dotenv()
 
 celery_app = Celery('main', broker=config.CELERY_BROKER)
 
