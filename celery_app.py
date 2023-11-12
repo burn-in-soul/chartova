59858e4d5b23e1c75cc587549a7bada74c9f3bc0
Alexander <csmcgl@mail.ru>
2023-11-13 01:07:26 +0300

    acks_late

--- celery_app.py
+++ celery_app.py
@@ -8,8 +8,7 @@ celery_app = Celery('main', broker=config.CELERY_BROKER,
 
 celery_app.conf.task_default_queue = 'chartova'
 
-
-@celery_app.task
+@celery_app.task(acks_late=True)
 def vote_pack(**kwargs) -> None:
     Voter().vote_pack(track_id=kwargs['track_id'])
     print('finish task')
