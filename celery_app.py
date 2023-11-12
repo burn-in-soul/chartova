b5942094a8f5b3099a58945205b8a8b6ca126c3a
Alexander <csmcgl@mail.ru>
2023-11-12 13:24:16 +0300

    celery queue

--- celery_app.py
+++ celery_app.py
@@ -10,6 +10,7 @@ celery_app = Celery('main', broker=config.CELERY_BROKER)
 
 celery_app.conf.task_default_queue = 'chartova'
 
+
 @celery_app.task
 def vote_pack(**kwargs) -> None:
     Voter().vote_pack(track_id=kwargs['track_id'])
