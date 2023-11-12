cffd9dfde15b2c4fabecc11547a033048e26029e
Alexander <csmcgl@mail.ru>
2023-11-13 01:21:23 +0300

    sec countdown

--- create_task.py
+++ create_task.py
@@ -12,9 +12,9 @@ def create_tasks(track_id: int, intervals: List[float]) -> None:
     countdown = 0
     for interval in intervals:
         vote_pack.apply_async(
-            eta=msc_tz.localize(datetime.now()) + timedelta(countdown),
+            eta=msc_tz.localize(datetime.now()) + timedelta(seconds=countdown),
             kwargs={'track_id': track_id},
             queue='chartova'
         )
-        print(msc_tz.localize(datetime.now()) + timedelta(countdown))
+        print(msc_tz.localize(datetime.now()) + timedelta(seconds=countdown))
         countdown += interval
