6b434a0ef5fd397e3e539b034f5a705607c9c9d6
Alexander <csmcgl@mail.ru>
2023-11-12 15:32:22 +0300

    Рестарт через pystemd

--- system_service/pystemd_service.py
+++ system_service/pystemd_service.py
@@ -5,4 +5,4 @@ class SystemdService:
 
     def restart(self, name: str) -> None:
         unit = Unit(f'{name}.service')
-        unit.Unit.Restart(mode=b'fail')
+        unit.Unit.Restart(mode=b's')
