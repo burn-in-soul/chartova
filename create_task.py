e3ec2b62fc5ab4a217fdfa695a1726cf5999b3f6
Alexander <csmcgl@mail.ru>
2023-11-13 01:11:19 +0300

    apply

--- create_task.py
+++ create_task.py
@@ -11,7 +11,7 @@ msc_tz = timezone('Europe/Moscow')
 def create_tasks(track_id: int, intervals: List[float]) -> None:
     countdown = 0
     for interval in intervals:
-        vote_pack.apply_async(
+        vote_pack.apply(
             eta=msc_tz.localize(datetime.now()) + timedelta(countdown),
             kwargs={'track_id': track_id},
             queue='chartova'
