f4795d1e6204e42920f69fece41e7671bd384d09
Alexander <csmcgl@mail.ru>
2023-11-13 01:18:50 +0300

    task_group

--- create_task.py
+++ create_task.py
@@ -1,6 +1,7 @@
 from datetime import datetime, timedelta
 from typing import List
 
+from celery import group
 from pytz import timezone
 
 from celery_app import vote_pack, test_task
@@ -10,10 +11,14 @@ msc_tz = timezone('Europe/Moscow')
 
 def create_tasks(track_id: int, intervals: List[float]) -> None:
     countdown = 0
+    task_group = []
     for interval in intervals:
-        vote_pack.apply(
-            eta=msc_tz.localize(datetime.now()) + timedelta(countdown),
-            kwargs={'track_id': track_id},
-            queue='chartova'
-        )
+        task_group.append(
+            vote_pack.s(
+                eta=msc_tz.localize(datetime.now()) + timedelta(countdown),
+                kwargs={'track_id': track_id},
+                queue='chartova'
+            ))
         countdown += interval
+    job = group(task_group)
+    job.apply_async()
