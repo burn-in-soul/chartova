d900414b4bbab0201b6dbc4781bf34c4f9196472
Alexander <csmcgl@mail.ru>
2023-11-13 00:10:51 +0300

    log finish

--- celery_app.py
+++ celery_app.py
@@ -11,6 +11,7 @@ celery_app.conf.task_default_queue = 'chartova'
 @celery_app.task
 def vote_pack(**kwargs) -> None:
     Voter().vote_pack(track_id=kwargs['track_id'])
+    print('finish task')
 
 
 @celery_app.task
